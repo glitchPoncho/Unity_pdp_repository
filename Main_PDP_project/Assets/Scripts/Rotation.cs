d3224257b4640f0aeacac8f833bbd8b047e2f6ee
glitchPoncho <bngosaurus@gmail.com>
2021-09-03 16:02:14 +0300

    Animation on objects

---
+++ Main_PDP_project/Assets/Scripts/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform Tree;
+    public float duration;
+    //public float scaleValue;
+    public Vector3 rotateAxis;
+    public Ease ease;
+    void Start()
+    {
+        Tree
+            .DORotate(rotateAxis, duration)
+            .SetEase(ease)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
---
+++ Main_PDP_project/Assets/Scripts/scaleTEst.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class scaleTEst : MonoBehaviour
+{
+    public Transform Bush;
+    public float duration;
+    public float scaleValue;
+    public Ease ease;
+    void Start()
+    {
+        Bush
+            .DOScale(scaleValue, duration)
+            .SetEase(ease)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
